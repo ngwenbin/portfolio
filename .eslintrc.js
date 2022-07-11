d1b4813dd041c269217e8bf9bda395bcfaec818a
wenbin <wenbin.ng@yahoo.com.sg>
2022-07-11 11:59:49 +0800

    added tailwindcss and updated import linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
-  parser: "@typescript-eslint/parser",
+  extends: ["plugin:react/recommended", "airbnb", "prettier", "@remix-run/eslint-config", "@remix-run/eslint-config/node"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -14,6 +13,17 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      "typescript": {
+        "alwaysTryTypes": true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        "project": "./tsconfig.json",
+      }
+    }
+  },
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/jsx-filename-extension": [
@@ -21,5 +31,7 @@ module.exports = {
       { extensions: [".js", ".jsx", ".ts", ".tsx"] },
     ],
     "prefer-template": "off",
+    "import/no-unresolved": "error"
   },
+
 };
