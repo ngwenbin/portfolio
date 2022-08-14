430a71d73fb562aee2f096809e96596b26b14095
Wen Bin <wenbin_ng@yahoo.com.sg>
2022-08-14 10:52:04 +0800

    feat: added terminal component and code refactor

---
+++ app/routes/root/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Hero } from "~/components";
+
+const RootIndex = () => (
+  <div className="sm:hidden">
+    <Hero />
+  </div>
+);
+
+export default RootIndex;
