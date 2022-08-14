798504cf257974d64202bd8fd80a6d2e2ad46253
Wen Bin <wenbin_ng@yahoo.com.sg>
2022-08-14 11:21:37 +0800

    chore: test padding on mobile

--- app/routes/root.tsx
+++ app/routes/root.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "@remix-run/react";
 
 const RootPage = () => {
   return (
-    <div className="block px-6 h-full w-full sm:max-w-[75%] mx-auto sm:flex py-10">
+    <div className="block px-6 h-full w-full sm:max-w-[75%] mx-auto sm:flex">
       <div className="flex flex-col sm:w-[400px] h-full justify-center">
         <div className="block sm:hidden grow">
           <Outlet />
