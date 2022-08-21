f944ee0a4de2cbbebc05e9cce6ef6b2fca79d50b
Wen Bin <wenbin_ng@yahoo.com.sg>
2022-08-21 14:24:08 +0800

    chore: added experience content and threedimensional wall component

--- app/routes/root.tsx
+++ app/routes/root.tsx
@@ -3,8 +3,8 @@ import { Outlet } from "@remix-run/react";
 
 const RootPage = () => {
   return (
-    <div className="grid grid-cols-1 px-6 h-full w-full sm:max-w-[75%] mx-auto lg:grid-cols-2 py-10">
-      <div className="flex flex-col h-full lg:max-w-[484px] justify-center p-5 gap-y-5">
+    <div className="grid grid-cols-1 px-6 h-full w-full sm:max-w-[75%] mx-auto lg:grid-cols-2 py-10 ">
+      <div className="flex flex-col h-full lg:max-w-[484px] justify-center p-5 gap-y-5 justify-self-end">
         <div className="lg:hidden grow">
           <Outlet />
         </div>
--- app/routes/root/experience.tsx
+++ app/routes/root/experience.tsx
@@ -1,5 +1,85 @@
 import React from "react";
 
-const ExperienceRoute = () => <div>ExperienceRoute</div>;
+interface ExperienceItem {
+  company: string;
+  position: string;
+  present: boolean;
+  location: string;
+  description: Array<string>;
+  startDate: string;
+  endDate?: string;
+}
+
+const experienceItems: Array<ExperienceItem> = [
+  {
+    company: "Trustana",
+    position: "Front-end Software Engineer Intern",
+    present: true,
+    location: "Singapore",
+    description: [
+      "Developed features for a B2B e-commerce SaaS product and internal tools using Remix, Tailwind, Prisma, GraphQL and Typescript in an agile environment.",
+    ],
+    startDate: "May 2022",
+  },
+  {
+    company: "Aerofoils GmbH",
+    position: "Embedded Software Engineer Intern",
+    present: false,
+    location: "Munich, Germany",
+    description: [
+      "Led development efforts of a GUI for an e-hydrofoil surfboard remote controller based on ESP32 running FreeRTOS using C and the LVGL library in ESP-ID",
+      "Built several interactive custom user interface widgets, screens, nested menus, and navigation logic",
+    ],
+    startDate: "Jan 2021",
+    endDate: "Jul 2021",
+  },
+  {
+    company: "B1G1 Pte.Ltd",
+    position: "Software Engineer Intern",
+    present: false,
+    location: "Singapore",
+    description: [
+      "Integrated Mixpanel, Google Analytics, Segment and ActiveCampaign into company's website and generated custom user behavior reports for internal stakeholders",
+      "Reduced sales team workload by automating marketing qualified leads (MQL) email verification processes by incorporating email verification API with ActiveCampaign.",
+    ],
+    startDate: "May 2020",
+    endDate: "Jul 2020",
+  },
+];
+
+const ExperienceRoute = () => (
+  <div className="flex flex-col gap-y-4 max-w-xl max-h-96 overflow-y-auto">
+    {experienceItems.map((exp, id) => {
+      return (
+        <div
+          key={id}
+          className="grid grid-cols-1 sm:grid-cols-3 p-8 rounded-xl text-sm bg-gray-300 text-black gap-y-2 w-full"
+        >
+          <div className="block col-span-2">
+            <p className="font-bold">{exp.company}</p>
+            <p className="font-medium">{exp.position}</p>
+          </div>
+          <div className="block lg:text-right">
+            <p>{exp.location}</p>
+            <p>
+              {exp.startDate} - {exp.endDate ?? "Present"}
+            </p>
+          </div>
+          <div className="col-span-2">
+            <ul>
+              {exp.description.map((item, idx) => {
+                return (
+                  <li key={idx} className="list-disc ml-8">
+                    <p className="text-xs font-light">{item}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      );
+    })}
+  </div>
+);
 
 export default ExperienceRoute;
