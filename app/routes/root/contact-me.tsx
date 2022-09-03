4747d5d84f01617a16ded72650149461dbc1889c
Wen Bin <wenbin_ng@yahoo.com.sg>
2022-09-03 20:20:15 +0800

    feat: integrated telegram bot for contact form

--- app/routes/root/contact-me.tsx
+++ app/routes/root/contact-me.tsx
@@ -1,6 +1,6 @@
-import { redirect } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import type { ActionFunction } from "@remix-run/node";
-import { useLocation, useNavigate } from "@remix-run/react";
+import { useActionData, useLocation, useNavigate } from "@remix-run/react";
 import { withZod } from "@remix-validated-form/with-zod";
 import { useEffect, useState } from "react";
 import { ValidatedForm } from "remix-validated-form";
@@ -8,14 +8,20 @@ import { z } from "zod";
 import { FormTextArea } from "~/components";
 import FormButton from "~/components/FormButton";
 import FormInput from "~/components/FormInput";
-import { CheckCircleIcon } from "@heroicons/react/outline";
+
+interface ActionData {
+  formError?: string;
+}
 
 const validator = withZod(
   z.object({
     name: z.string().min(1, "Name is required."),
     company: z.string().min(1, "Company is required."),
     email: z.string().min(1, "Email is required."),
-    message: z.string().min(1, "Message is required."),
+    message: z
+      .string()
+      .min(1, "Message is required.")
+      .max(500, "Message too long."),
   })
 );
 
@@ -30,7 +36,27 @@ const extractFormData = (formData: any) => {
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
-  return redirect("/root/contact-me?sucess");
+  const apiKey = process.env.BOT_API;
+  const uidKey = process.env.USER_ID;
+  const data = extractFormData(formData);
+  const formattedText = `Name: ${data.name}\nCompany: ${data.company}\nEmail: ${data.email}\nMessage: ${data.message}`;
+  const endPoint = `https://api.telegram.org/${apiKey}/sendMessage?text=${encodeURIComponent(
+    formattedText
+  )}&chat_id=${uidKey}`;
+
+  try {
+    const response = await fetch(endPoint, {
+      method: "POST",
+    });
+    if (response.ok) {
+      return redirect("/root/contact-me?success");
+    }
+    throw Error(`${response.status} ${response.statusText}`);
+  } catch (error: any) {
+    return json({
+      formError: error.message,
+    });
+  }
 };
 
 const ContactMeRoute = () => {
@@ -38,58 +64,66 @@ const ContactMeRoute = () => {
   const { search } = useLocation();
   const navigate = useNavigate();
   const searchParams = new URLSearchParams(search);
+  const actionData = useActionData() as ActionData;
+  const [errorMsg, setErrorMsg] = useState<string>("");
 
   useEffect(() => {
-    if (searchParams.has("sucess")) {
+    if (searchParams.has("success")) {
       setSuccess(true);
       navigate("/root/contact-me");
     }
   }, [searchParams]);
 
+  useEffect(() => {
+    if (actionData && actionData?.formError) {
+      setErrorMsg(actionData.formError);
+    }
+  }, [actionData]);
+
   return (
     <div className="w-full max-w-sm">
-      {success ? (
-        <div className="flex items-center gap-x-2 text-xl">
-          <CheckCircleIcon width={32} height={32} color="green" />
-          Success!
+      <ValidatedForm validator={validator} method="post">
+        <div className="grid grid-cols w-full gap-y-2">
+          <FormInput
+            id="name"
+            name="name"
+            label="Name*"
+            autoComplete="off"
+            placeholder="Your name"
+          />
+          <FormInput
+            id="company"
+            name="company"
+            label="Company*"
+            autoComplete="off"
+            placeholder="Your company"
+          />
+          <FormInput
+            id="email"
+            name="email"
+            label="Email*"
+            autoComplete="off"
+            placeholder="Your email"
+          />
+          <FormTextArea
+            id="message"
+            name="message"
+            label="Message*"
+            autoComplete="off"
+            placeholder="Your message"
+          />
+          <FormButton
+            className="px-4 py-3 text-sm font-medium leading-4 rounded-lg place-self-end bg-cyan-500 disabled:opacity-50"
+            onClick={() => setErrorMsg("")}
+          >
+            Submit
+          </FormButton>
+          {errorMsg && <p className="text-red-500">{errorMsg}</p>}
+          {success && (
+            <p className="flex items-center gap-x-2 text-green-500">Success!</p>
+          )}
         </div>
-      ) : (
-        <ValidatedForm validator={validator} method="post">
-          <div className="grid grid-cols w-full gap-y-2">
-            <FormInput
-              id="name"
-              name="name"
-              label="Name"
-              autoComplete="off"
-              placeholder="Your name"
-            />
-            <FormInput
-              id="company"
-              name="company"
-              label="Company"
-              autoComplete="off"
-              placeholder="Your company"
-            />
-            <FormInput
-              id="email"
-              name="email"
-              label="Email"
-              autoComplete="off"
-              placeholder="Your email"
-            />
-            <FormTextArea
-              id="message"
-              name="message"
-              label="Message"
-              autoComplete="off"
-              placeholder="Your message"
-            />
-            <FormButton className="px-4 py-3 text-sm font-medium leading-4 rounded-lg place-self-end bg-cyan-500 shadow-lg shadow-cyan-500/50 disabled:opacity-50">
-              Submit
-            </FormButton>
-          </div>
-        </ValidatedForm>
-      )}
+      </ValidatedForm>
     </div>
   );
 };
