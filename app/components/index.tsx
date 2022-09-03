ba9533ae2c4fb7375e6e085a4a6cdb31e0b812e7
Wen Bin <wenbin_ng@yahoo.com.sg>
2022-09-03 16:20:51 +0800

    feat: added modal component

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -3,3 +3,4 @@ export type { NavItem } from "./Navbar";
 export { default as Terminal } from "./Terminal";
 export { default as Hero } from "./Hero";
 export { default as SphericalNode } from "./SphericalNode";
+export { default as Modal } from "./Modal";
--- app/routes/root/projects.tsx
+++ app/routes/root/projects.tsx
@@ -1,5 +1,63 @@
 import React from "react";
+import FolderSvg from "~/images/FolderSvg.webp";
 
-const ProjectsRoute = () => <div>ProjectsRoute</div>;
+interface ProjectItem {
+  name: string;
+  details: ProjectDetails;
+}
+
+interface ProjectDetails {
+  title: string;
+  description: string;
+  skills: Array<string>;
+  link: string;
+}
+
+const projectItems: Array<ProjectItem> = [
+  {
+    name: "Recyclables",
+    details: {
+      title: "Rag and bone man hailing telegram bot",
+      description:
+        "Enables Singapore residents to hail for a rag and bone man via a telegram bot. Collected 4.2 tons of recyclables with over 780 users.",
+      skills: ["Python", "Javascript", "NoSQL (Firestore)", "Cloud functions"],
+      link: "https://recyclables.netlify.app/",
+    },
+  },
+  {
+    name: "Impact giving map",
+    details: {
+      title: "Impact giving map",
+      description: "Built an interactive 3D map using deck.gl",
+      skills: ["React", "Deck.gl", "REST API"],
+      link: "https://b1g1impactmaptest.herokuapp.com/",
+    },
+  },
+];
+
+const ProjectsRoute = () => {
+  return (
+    <div>
+      <div className="flex items-center gap-x-4">
+        {projectItems.map((item, idx) => {
+          return (
+            <div
+              key={idx}
+              className="flex flex-col items-center max-w-[120px] group"
+              // onClick={() => console.log("ok")}
+            >
+              <img
+                src={FolderSvg}
+                className="group-hover:scale-90 cursor-pointer active:opacity-50"
+                alt="folder"
+              />
+              <p className="text-xs font-base text-center">{item.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default ProjectsRoute;
